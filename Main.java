24e4dab256288591662d66400f99d46debb4cf9b
Miguel9137 <mr9137@bard.edu>
2020-09-03 22:33:40 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+// please don't edit this one, fork it
+
+import edu.princeton.cs.algs4.*;
+
+class Main {
+  public static void main(String[] args) {
+    StdOut.println("Hi, what's your last name?");
+    String name = StdIn.readString();
+    StdOut.println("Nice to meet you" + name);
+  }
+}
